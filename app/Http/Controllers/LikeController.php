03571304844b689d88ee2d6c1bf293e2fe081d86
GeorgeBarhoum <georges.barhoum@scriptr.io>
2020-11-15 01:10:23 +0200

    test1

---
+++ app/Http/Controllers/LikeController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Http\Controllers\CategoryController;
+use App\Models\Articles;
+use App\Models\Category;
+use App\Models\Like;
+use Illuminate\Http\Request;
+
+class LikeController extends Controller
+{
+    public function __invoke($slug)
+    {
+        //get the requested like
+        $like = Like::query()
+            ->where('slug', $slug)
+            ->firstOrFail();
+
+        //get the articles with that like
+        $articles = $like->articles()
+            ->where('is_published',true)
+            ->orderBy('id','desc')
+            ->paginate(env('PAGE_NUMBER'));
+
+        //get all the categories
+        $categories = Category::all();
+
+        //get all the likes
+        $likes = Like::all();
+
+        //get the recent 5 articles
+        $recent_articles = Articles::query()
+            ->where('is_published',true)
+            ->orderBy('created_at','desc')
+            ->take(5)
+            ->get();
+
+        //return the data to the corresponding view
+        return view('like', [
+            'like' => $like,
+            'articles' => $articles,
+            'categories' => $categories,
+            'likes' => $likes,
+            'recent_articles' => $recent_articles
+        ]);
+    }
+}
+
+
+Article::create($request->all()); 
+@if ($message = Session::get('success')) <div class="alert alert-success"> <p>{{ $message }}</p> </div> @endif 
---
+++ resources/views/article/index.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+
+@section('content')
+    
+@if ($message = Session::get('success')) 
+<div class="alert alert-success"> 
+    <p>{{ $message }}</p> 
+</div> 
+@endif
+        @foreach ($articles as $article)
+            <div>
+                <h3>
+                    {{ $article->title }}
+                </h3>
+                <p>
+                    {{ $article->body }}
+                </p>
+                <p>
+                    {{ $article->user->name }}
+                </p>
+            </div>
+            
+        @endforeach
+
+@endsection
